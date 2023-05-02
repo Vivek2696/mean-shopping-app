28c075de65cdf3c8aecfb120e211c541ebbf7144
Vivek Patel <vivekdp2@illionis.edu>
2023-05-02 00:19:27 -0400

    Adding the angular material and toolbar for the app

---
+++ front-end/shopping-front-app/src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+  ]
+})
+export class MaterialModule { }
